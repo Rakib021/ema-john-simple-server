cfe9ec6e8fd8cbcb9d2f9eb677bd51a3ba8a9a0b
Rakib <rakib@Rakibs-MacBook-Pro.local>
2022-09-26 00:06:51 +0600

    createing get api for practice

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send("john is running and waiting for ema");
+})
+
+app.listen(port,() =>{
+    console.log("John is running on port" ,port);
+})
